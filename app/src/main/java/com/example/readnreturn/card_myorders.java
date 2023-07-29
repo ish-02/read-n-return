d79c7e1220aaad4ff658842a80127df80a702dfa
Navpute Bhakti Bhaskar <96111656+Bnavpute@users.noreply.github.com>
2023-07-29 16:36:43 +0530

    added cards for myUploads and myOrders page

---
+++ app/src/main/java/com/example/readnreturn/card_myorders.java
@@ -0,0 +1,14 @@
+package com.example.readnreturn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class card_myorders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_myorders);
+    }
+}
