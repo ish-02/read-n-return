df1b400728321185219721a92dbc3aadbb67a1a9
Navpute Bhakti Bhaskar <96111656+Bnavpute@users.noreply.github.com>
2023-07-29 19:46:02 +0530

    added pages for myUploads and myOrders page

---
+++ app/src/main/java/com/example/readnreturn/MyUploadsPage.java
@@ -0,0 +1,14 @@
+package com.example.readnreturn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyUploadsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_uploads_page);
+    }
+}
