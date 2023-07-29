226262c34ec8d7d9ff41fbc23dd660cb393e7db8
Navpute Bhakti Bhaskar <96111656+Bnavpute@users.noreply.github.com>
2023-07-29 14:02:21 +0530

    added profile page and order book page.

---
+++ app/src/main/java/com/example/readnreturn/OrderbookActivity.java
@@ -0,0 +1,14 @@
+package com.example.readnreturn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderbookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.orderbook);
+    }
+}
